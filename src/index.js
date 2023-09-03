cd010b423e66b2849f812af4a9c93c639dca622c
khoi le <lmkhoi2k@gmail.com>
2023-09-03 11:41:00 +0700

    Route & Controller

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -19,14 +25,9 @@ app.set("view engine", "hbs");
 
 app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
