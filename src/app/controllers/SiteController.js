f24aa4b52170a547e4426ce226beb9b7cd7c31ba
khoi le <lmkhoi2k@gmail.com>
2023-09-07 11:10:26 +0700

    config prettier & [MVC] Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,21 @@
+const Course = require('../models/Course')
 class SiteController {
-   
-    // [GET] /
-    index(req, res) {
-       res.render('home');
-    }
- 
-    // [GET] /search
-    search(req, res) {
-       res.render('search');
-    }
- }
- 
- module.exports = new SiteController;
+  // [GET] /
+  index(req, res) {
+    //  res.render('home');
+    Course.find()
+      .then((courses, err) => {
+        res.json(courses)
+      })
+      .catch(() => {
+        res.status(400).json({ error: 'ERROR!!!' })
+      })
+  }
+
+  // [GET] /search
+  search(req, res) {
+    res.render('search')
+  }
+}
+
+module.exports = new SiteController()
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
 
+const route = require("./routes");
+const db = require("./config/db");
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({
@@ -29,5 +34,5 @@ app.set("views", path.join(__dirname, "resources", "views"));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`App listening on port http://localhost:${port}`);
 });
