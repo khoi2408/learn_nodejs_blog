9da15ee5150afbf799aa267f4f50cc52ce25ca24
khoi le <lmkhoi2k@gmail.com>
2023-09-09 17:23:46 +0700

    Handle update

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -20,6 +20,22 @@ class CourseController {
         res.render('courses/create')
     }
 
+    // [PUT] /courses/:id
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next)
+    }
+
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then(course => res.render('courses/edit', {
+                course: mongooseToObject(course)
+            }))
+            .catch(next)
+    }
+
     // [POST] /courses/store
     store(req, res, next) {
         const formData = req.body
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
+const methodOverride = require('method-override');
 const path = require("path");
 
 const route = require("./routes");
@@ -19,12 +20,17 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
 app.engine("hbs", handlebars.engine({
   extname: ".hbs",
+  helpers: {
+    sum: (a, b) => a + b,
+  },
 }));
 app.set("view engine", "hbs");
 
